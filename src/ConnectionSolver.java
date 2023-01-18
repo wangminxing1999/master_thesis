4310d84010993408d6e452b80f0d4742110bc362
wang minxing <ge32ciq>
2023-01-18 23:48:27 +0100

    initialization finished

--- src/ConnectionSolver.java
+++ src/ConnectionSolver.java
@@ -20,20 +20,33 @@ public class ConnectionSolver {
         this.connection_wrapper = new ConnectionWrapper[chip_request.getConnections().size()];
         this.NumOfSegments = NumOfSegments;
     }
-
-    public void solve_connection_wrapper() throws GRBException {
+    public void initilization_connection_wrapper() throws GRBException {
         for(int i = 0; i < this.connection_wrapper.length; i++) {
             this.connection_wrapper[i] = new ConnectionWrapper(this.chip_request.getConnections().get(i), NumOfSegments);
-            if(this.connection_wrapper[i].getConnection().getPin1().getModule().getLayer() == this.connection_wrapper[i].getConnection().getPin2().getModule().getLayer()) {
-                for(solve_ChannelSegment channel_segment : this.connection_wrapper[i].channel_segment) {
-                    IntegerVariable x1 = model.intVar("x1", 0, chip.getWidth());
-                    IntegerVariable y1 = model.intVar("y1", 0, chip.getHeight());
-                    IntegerVariable x2 = model.intVar("x2", 0, chip.getWidth());
-                    IntegerVariable y2 = model.intVar("y2", 0, chip.getHeight());
-                    channel_segment = new solve_ChannelSegment(x1, y1, x2, y2, this.connection_wrapper[i].getConnection(),this.connection_wrapper[i].getConnection().getPin1().getModule().getLayer());
+            for(int j = 0; j < this.connection_wrapper[i].channel_segment.length; j++){
+                IntegerVariable x1 = model.intVar("x1", 0, chip.getWidth());
+                IntegerVariable y1 = model.intVar("y1", 0, chip.getHeight());
+                IntegerVariable x2 = model.intVar("x2", 0, chip.getWidth());
+                IntegerVariable y2 = model.intVar("y2", 0, chip.getHeight());
+                if(j < this.connection_wrapper[i].channel_segment.length/2)
+                    this.connection_wrapper[i].channel_segment[j] = new solve_ChannelSegment(x1, y1, x2, y2, this.connection_wrapper[i].getConnection(),this.connection_wrapper[i].getConnection().getPin1().getModule().getLayer());
+                else {
+                    this.connection_wrapper[i].channel_segment[j] = new solve_ChannelSegment(x1, y1, x2, y2, this.connection_wrapper[i].getConnection(),this.connection_wrapper[i].getConnection().getPin2().getModule().getLayer());
+                }
+                if(this.connection_wrapper[i].getConnection().getPin1().getModule().getLayer() != this.connection_wrapper[i].getConnection().getPin2().getModule().getLayer()) {
+                    IntegerVariable x = model.intVar("x",0,chip.getWidth());
+                    IntegerVariable y = model.intVar("y",0,chip.getHeight());
+                    this.connection_wrapper[i].via = new solve_Via(this.connection_wrapper[i].getConnection().getPin1().getModule().getLayer(),this.connection_wrapper[i].getConnection().getPin2().getModule().getLayer(), x, y);
                 }
             }
         }
+    }
+
+    public void add_single_channelsegment_position_constraint() {
+
+    }
+    public void solve_connection_wrapper() {
+
     }
 
     public Chip getChip() {return this.chip;}
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
+        int a[] = {1,2,3,4};
+        for( int i = 0; i < a.length; i++) {
+            a[i] ++;
+        }
+        System.out.println(a[0]);
     }
 }
